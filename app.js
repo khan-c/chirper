18c1d47d3b2a24814a1461b236befa5fd4604d26
Kyle Chen <kyle.h.chen@gmail.com>
2019-11-10 17:41:42 -0800

    Set up some initial routes

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
-  .connect(db, { userNewUrlParser: true })
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => {
+  console.log(res);
+  res.send("Hello World");
+});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
